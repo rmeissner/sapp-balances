ab92100d8d0bd16d482057dab8a532cea3c89626
Richard Meissner <msc.meissner@gmail.com>
2020-11-21 02:56:05 +0100

    Updates

---
+++ precache-manifest.a4eccab367143cf6532fb93cce7a893f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "315a0bc39cc09b07b907f79029883e39",
+    "url": "./index.html"
+  },
+  {
+    "revision": "323cf4c6c62fe1392d52",
+    "url": "./static/css/main.5e49ad75.chunk.css"
+  },
+  {
+    "revision": "f63dbe5c6d92171c375e",
+    "url": "./static/js/2.f032f215.chunk.js"
+  },
+  {
+    "revision": "c66bb4717f438256af35ec5bddca2eb7",
+    "url": "./static/js/2.f032f215.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "323cf4c6c62fe1392d52",
+    "url": "./static/js/main.b63cde46.chunk.js"
+  },
+  {
+    "revision": "5c470ddaf0aa669ee84b",
+    "url": "./static/js/runtime-main.d52ca872.js"
+  }
+]);
